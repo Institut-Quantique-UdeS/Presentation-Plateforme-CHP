a99f263159a0254d7d602487d7632e4cd134d26b
Olivier Fisette <olivier.fisette@usherbrooke.ca>
2024-10-04 13:24:32 -0400

    Changement de la police de caractères

--- en/source/conf.py
+++ en/source/conf.py
@@ -58,3 +58,10 @@ html_title = project
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+html_theme_options = {
+    "light_css_variables": {
+        "font-stack": "Lato, serif",
+        "font-stack--headings": "Lato, serif",
+    }
+}
